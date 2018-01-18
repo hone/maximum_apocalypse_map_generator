107ad6086d5b36ef79e95bd2ec0bcc0172dae038
Terence Lee <hone02@gmail.com>
2018-01-18 14:26:22 -0600

    add CLI binary

---
+++ src/bin.rs
@@ -0,0 +1,21 @@
+extern crate ma_mapper;
+
+use std::io;
+use std::io::prelude::*;                                                           
+use ma_mapper::board::Board;
+
+fn main() {
+    let mut input = String::new();
+
+    print!("Enter Number of Tiles: ");
+    io::stdout().flush().ok().expect("Could not flush stdout");
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            match input.trim().parse::<usize>() {
+                Ok(num_tiles) => Board::new(&num_tiles).display(),
+                Err(error) => eprintln!("Not a valid number: error: {}", error),
+            };
+        },
+        Err(error) => eprintln!("Error getting input: {}", error),
+    };
+}
